cd964a683b612af7f08ee0a5f91cfa951cfba8cc
shivaraj110 <shivarajchandaragi9@gmail.com>
2026-01-26 12:35:42 +0530

    updated rice

--- lua/plugins/richPresence.lua
+++ lua/plugins/richPresence.lua
@@ -7,15 +7,15 @@ return {
       neovim_image_text = "Neovim",
       main_image = "neovim",
       debounce_timeout = 10,
-      enable_line_number = true,
+      enable_line_number = false,
       show_time = true,
-
+      enable_workspace = true,
       editing_text = "Editing %s",
       file_explorer_text = "Browsing files",
       git_commit_text = "Committing changes",
       plugin_manager_text = "Managing plugins",
       reading_text = "Reading %s",
-      workspace_text = "Working on %s",
+      workspace_text = "Cooking in %s",
       line_number_text = "Line %s out of %s",
     })
   end,
